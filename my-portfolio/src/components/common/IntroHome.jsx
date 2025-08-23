b7b8c6552862b1a8197a8b23f3acc769d35f3e3e
meghs <saiimeghanaa9@gmail.com>
2025-08-23 16:56:24 +0530

    navbar-icon and button changes completed

--- my-portfolio/src/components/common/IntroHome.jsx
+++ my-portfolio/src/components/common/IntroHome.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import { LuGithub } from "react-icons/lu";
+import { LuLinkedin } from "react-icons/lu";
+import { LuMail } from "react-icons/lu";
 
 
 function IntroHome() {
     return(
         <div className='hero'>
             <section>
-            <p classsName='hero-description'>Hello, I am</p>
+            <p className='hero-description'>Hello, I am</p>
             <p className='hero-subtitle'>Meghanaa</p>
             <p className='hero-subtitle'>A Frontend Developer</p>               
             <p className='hero-description'>I design and code beautifully simple things, and I love what I do.</p>
-            <button className='button-primary'>View my work</button> 
+            <button className='button-primary'>View my work </button> 
+            <button className='button-outline'>Get in touch</button>
             </section>
+            <div className='hero-socials'>
+                <LuGithub/>
+                <LuLinkedin />
+                <LuMail />
+            </div>
         </div>
     )
 }
